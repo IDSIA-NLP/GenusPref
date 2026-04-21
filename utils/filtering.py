2e86e7043eaad3136e5b0d44f97d873e8d868e9f
davkletz <46730051+davkletz@users.noreply.github.com>
2026-04-21 19:44:28 +0200

    Updated for Switchscores

--- utils/filtering.py
+++
@@ -1,4 +0,0 @@
-
-def filter_languages_by_threshold(nb_corr_answ_lg, threshold):
-    """Filter languages that have at least threshold correct answers."""
-    return [lg for lg in nb_corr_answ_lg if nb_corr_answ_lg[lg] >= threshold]
